7337b3501ddf873493956f1192cc761288795c00
Dominika Barrett <barrettdomi@gmail.com>
2020-08-30 21:09:21 +0100

    created get_move finction with VE

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,64 +1,19 @@
-board = [["_" for _ in range(3)] for _ in range(3)]
-
-
-def init_board():
-    """Returns an empty 3-by-3 board (with .)."""
-    board = []
-    return board
-
-
-def get_move(board, player):
-    """Returns the coordinates of a valid move for player on board."""
-    row, col = 0, 0
-    return row, col
-
-
-def get_ai_move(board, player):
-    """Returns the coordinates of a valid move for player on board."""
-    row, col = 0, 0
-    return row, col
-
-
-def mark(board, player, row, col):
-    """Marks the element at row & col on the board for player."""
-    pass
-
-
-def has_won(board, player):
-    """Returns True if player has won the game."""
-    return False
-
-
-def is_full(board):
-    """Returns True if board is full."""
-    return False
-
-
 def print_board(board):
-    """Prints a 3-by-3 board on the screen with borders."""
-    pass
+    for row in board:
+        print(row)
+
+#wybieram pole od 1 do 9 (input always returns a string!)
+def get_move():
+    move = int(input("Wybierz pole: "))
+    if not 1 <= move <= 9:
+        raise ValueError
+    return move
 
 
-def print_result(winner):
-    """Congratulates winner or proclaims tie (if winner equals zero)."""
-    pass
-
-
-def tictactoe_game(mode='HUMAN-HUMAN'):
-    board = init_board()
-
-    # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
-    print_board(board)
-    row, col = get_move(board, 1)
-    mark(board, 1, row, col)
-
-    winner = 0
-    print_result(winner)
-
-
-def main_menu():
-    tictactoe_game('HUMAN-HUMAN')
-
-
-if __name__ == '__main__':
-    main_menu()
+#definiuje tablice jako liste list i wykluczam error
+board = [["_" for _ in range(3)] for _ in range(3)]
+try:
+    move = get_move()
+    print(move)
+except ValueError:
+    print("Prosze wybrac z przedzialu od 1-9")

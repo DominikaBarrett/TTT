fb4f238ac22e3cd27d1612c59996d3eba2b46e28
Dominika Barrett <barrettdomi@gmail.com>
2020-09-06 13:10:06 +0100

    added final code

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,42 +2,51 @@
 #definiuje tablice jako liste list i wykluczam error
 #zmieniam zakres odejmujac jeden na 0-8
 #dzielac uzywajac modulo zwracam krotke definujac i/j jako kolumny i wiersze
+import sys
+import os
+
+
+def clear_terminal():
+    os.system("cls || clear")
+
+
 def main():
+    user_name()
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print_instructions()
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     board = [["_" for _ in range(3)] for _ in range(3)]
     player_x = True
     game_over = False
+
     while not game_over:
+
+        clear_terminal()
         print_board(board)
+
 #tak wyglada tablica pod"przykrywka"
 #board = [
-#   j          j     j
-# i[0 | 0,  0  |  1,  0 | 2],
-# i[1 |  0, 1  |  1,  1 | 2],
-# i[2 | 0,  2 | 1,  2 | 2]
-# ]
-# przyklady
-# 1 = > 0,0
-# 4 = > 1,0
-# 8 => 2,1
+#   j      j      j
+# i[0|0,  0|1,  0|2],
+# i[1|0,  1|1,  1|2],
+# i[2|0,  2|1,  2|2]]
+
         try:
             move = convert_move(choose_move())
-            if move != (-1, -1):
-                get_move(move, player_x, board)
-            else:
-                print("Podaj wartość z zakresu od 1 do 9!")
+            get_move(move, player_x, board)
         except ValueError:
-            print("To miejsce jest juz zajete, wybierz ponownie...")
+            print("-------------------------------------")
+            print("Podaj cyfre określająca pole od 1 do 9. ")
+            print("-------------------------------------")
             continue
         game_over = is_win(board) or is_draw(board)
         player_x = not player_x
+    play_again()
 
 
 def convert_move(move):
-    if move > -1:
-        move -= 1
-        return move // 3, move % 3
-    else:
-        return -1, -1
+    move -= 1
+    return move // 3, move % 3
 
 
 def is_draw(board):
@@ -45,6 +54,7 @@ def is_draw(board):
         for val in row:
             if val == "_":
                 return False
+    print_board(board)
     print("REMIS!!!")
     return True
 
@@ -66,19 +76,22 @@ def is_win(board):
             winner = board[1][1]
         if winner is not None:
             print_board(board)
-            print(f"{winner} jetses zwyciesca!!!")
+            print("-----------")
+            print(f"Gratulacje {winner} jesteś zwycięzcą!!")
+            print("-----------")
             return True
         return False
 
 
 def get_move(move, player_x, board):
-    #sprawdzam czy tablica jest pusta zanim ja wypelnoe x /y
+    #sprawdzam czy tablica jest pusta
     #move jest tuple
     i, j = move
     if board[i][j] == "_":
         board[i][j] = "X" if player_x else "O"
     else:
-        raise ValueError
+        # raise ValueError
+        print("To miejsce jest już zajęte., wybierz ponownie")
 
 
 def print_board(board):
@@ -90,11 +103,11 @@ def print_board(board):
     numbers = 1
     for row in board:
         if counter == 0:
-            template = f'    \033[36m{counter + 1}\033[0m | {counter + 2} | {counter + 3}'
+            template = f'    \033[36m{counter + 1}\033[0m |\033[36m{counter + 2}\033[0m | \033[36m{counter + 3}\033[0m'
         elif counter == 1:
-            template = f'    {counter + 3} | {counter + 4} | {counter + 5}'
+            template = f'   \033[36m{counter + 3}\033[0m | \033[36m{counter + 4}\033[0m | \033[36m{counter + 5}\033[0m'
         else:
-            template = f'    {counter + 5} | {counter + 6} | {counter + 7}'
+            template = f'   \033[36m {counter + 5} | \033[36m{counter + 6}\033[0m | \033[36m{counter + 7}\033[0m'
         print(f' {row[cell_0]} | {row[cell_1]} | {row[cell_2]} {template}')
         if counter < 2:
             print(f'{line}   {line}')
@@ -106,14 +119,35 @@ def print_board(board):
 
 
 def choose_move():
-    try:
+    print("++++++++++++")
+    move = int(input("Wybierz pole: "))
+    print("++++++++++++")
+    while move < 1 or move > 9:
+        print("Wybierz poprawne pole, tak jak sugeruje rysunek")
         move = int(input("Wybierz pole: "))
-        if 1 <= move <= 9:
-            return move
-        else:
-            return -1
-    except ValueError:
-        return -1
+    return move
+
+
+def print_instructions():
+    print("Witamy w naszej grze: Kółko i krzyżyk.\n Zasady gry sa bardzo proste, wybierasz ruch od 1 do 9.\n Wygrywa madrzejszy ;) ")
+
+
+def user_name():
+    name = input('Jak masz na imię?...')
+    hi = input("Czy chcesz zagrać Kółko i krzyżyk? t/n")
+    if hi == "tak" or hi == "t":
+        print("Cześć " + name, "Pozwole Ci zacząć ..")
+    else:
+        sys.exit()
+
+
+def play_again():
+    question = input("Chcesz zagrać ponownie? t/n")
+    if question == "t" or question == "tak":
+        return main()
+    else:
+        print("Miłego dnia !!")
+        sys.exit()
 
 
 if __name__ == "__main__":
